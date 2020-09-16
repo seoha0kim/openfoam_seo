4881d80670f6be73983485fbd3a244a1b548c2ae
sbkim <seroBNES@gmail.com>
2020-09-17 04:22:09 +0900

    s1

--- wtt/yjn2/imsi.m
+++ wtt/yjn2/imsi.m
@@ -2,7 +2,7 @@ function out = model
 %
 % imsi.m
 %
-% Model exported on Sep 16 2020, 22:40 by COMSOL 5.5.0.359.
+% Model exported on Sep 17 2020, 04:21 by COMSOL 5.5.0.359.
 
 import com.comsol.model.*
 import com.comsol.model.util.*
@@ -23,17 +23,24 @@ model.study.create('std1');
 model.study('std1').create('stat', 'Stationary');
 model.study('std1').feature('stat').activate('spf', true);
 
-model.label('rib_imsi.mph');
-
 model.component('comp1').geom('geom1').create('imp1', 'Import');
+model.component('comp1').geom('geom1').feature('imp1').set('type', 'dxf');
 model.component('comp1').geom('geom1').feature('imp1').set('filename', '/media/sbkim/2266B8F966B8CEB3/git/openfoam_seo/wtt/yjn2/yjn2_cfd_rib_200915.dxf');
 model.component('comp1').geom('geom1').feature('imp1').set('knit', 'curve');
-model.component('comp1').geom('geom1').runPre('fin');
-model.component('comp1').geom('geom1').run('imp1');
 model.component('comp1').geom('geom1').create('csol1', 'ConvertToSolid');
 model.component('comp1').geom('geom1').feature('csol1').selection('input').set({'imp1'});
-model.component('comp1').geom('geom1').runPre('fin');
+
+model.label('rib_imsi.mph');
+
+model.component('comp1').geom('geom1').run('csol1');
+model.component('comp1').geom('geom1').run('csol1');
+model.component('comp1').geom('geom1').create('del1', 'Delete');
+model.component('comp1').geom('geom1').feature('del1').selection('input').init(2);
+model.component('comp1').geom('geom1').feature('del1').selection('input').set('csol1', [1 2 3 4 5 16 17 18 19 20]);
+model.component('comp1').geom('geom1').run('del1');
+model.component('comp1').geom('geom1').measure.selection.init(2);
+model.component('comp1').geom('geom1').measure.selection.all('del1(1)');
 model.component('comp1').geom('geom1').measure.selection.init(0);
-model.component('comp1').geom('geom1').measure.selection.set('csol1', [2 63]);
+model.component('comp1').geom('geom1').measure.selection.set('del1', [1 32]);
 
 out = model;
--- wtt/yjn2/rib_200915.m
+++ wtt/yjn2/rib_200915.m
@@ -58,7 +58,26 @@ id_pl = false;
 % ## Blockage ratio
 
 % %% [markdown]
-% - 3 - 5 %
+% $$\mathrm{
+% % \begin{empheq}[left=\empheqlbrace]{align}
+% \left\{
+% aa
+% \right.
+% % \end{empheq}
+% }$$
+
+% %% [markdown]
+% $$\mathrm{
+% % \begin{empheq}[left=\empheqlbrace]{align}
+% \left\{
+% \begin{aligned}\mathrm{
+%  < 3 - 5 \% : recommended \\
+%  < 10 \% : Dr. Kwon, Dae \, Kun \\
+%  > 10 \% : correction}
+% \end{aligned}
+% \right.
+% % \end{empheq}
+% }$$
 
 % %% [markdown]
 % ## Small wind tunnel for 2d section models
@@ -149,7 +168,6 @@ model.study('std1').feature('stat').activate('spf', true);
 % mphsave(model,'rib_imsi')
 
 % %%
-
 % model.component('comp1').geom('geom1').create('imp1', 'Import');
 % model.component('comp1').geom('geom1').feature('imp1').set('filename', ...
     % '/home/sbkim/Work/git/openfoam_seo/wtt/yjn2/yjn2_cfd_deck_200915.dxf');
@@ -170,6 +188,8 @@ model.component('comp1').geom('geom1').create('csol1', 'ConvertToSolid');
 % model.component('comp1').geom('geom1').feature('csol1').selection('input').set({ ...
     % 'imp1(2)' 'imp1(3)' 'imp1(4)' 'imp1(5)'});
 model.component('comp1').geom('geom1').feature('csol1').selection('input').set({'imp1'});
+
+% %%
 model.component('comp1').geom('geom1').create('del1', 'Delete');
 model.component('comp1').geom('geom1').feature('del1').selection('input').set('imp1(1)', [1 3]);
 model.component('comp1').geom('geom1').feature('del1').selection('input').set('csol1(1)', [1 2 3 4 5 6 7 19 20 21 29 30 31 32 37 38 39 40 41 42 43 51 52 53 54 55 57 58 59 62 66 70 72 75 76 78 80 83 84 85 87 89 92 93 94 96 98 101 102 104 106 109 110 112 114 115 116 117 118 125 127 128 129 130 131 132 133 134 135 136 137 138 139 140 141 142 143 145 146 158 165 171 172 174 176 179 180 182 184 187 188 190 192 195 196 198 200 203 204 206 208 211 212 214 216 219 220 222 224 227 228 230 232 235 236 238 239 241 245 246 248 250 253 254 256 258 261 262 264 266 269 270 272 274 277 278 280 282 285 286 288 290 293 294 296 298 301 302 304 306 309 310 312 313 314 315 317 318 319 320 321 322 323 324 325 326 327 329 330 331 332 333 334 335 337 338 340 341 363 364 365 366 367 368 369 370 371 372 373 374 377 389 390 391 407 408 409 411 412 413 414 415 416 417 418 419 420 421 422 423 424 425 426 427 428 429 430 431 432 433 434 435 436 439 440 443 444 459 470 473 474 477 478 481 482 485 486 489 490 493 494 495 496 497 498 499 500 501 502 503 504 505 506 511 523 524 527 528 531 532 535 536 539 540 543 544 547 548 551 552 555 556 559 560 563 564 567 568 571 572 575 576 579 580 583 584 587 588 591 592 594 595 596 597 598 599 600 601 602 603 604 605 607 608 609 610 611 612]);
@@ -177,6 +197,12 @@ model.component('comp1').geom('geom1').feature('del1').selection('input').set('c
 % Point 1 (del1(2)) to 320 (del1(2)).
 % Average coordinates: (157333.0083, -1067.977166) [m].
 % Points: 1, 320 (del1(2)).
+
+
+% [Sep 17, 2020 4:20 AM] 
+% Distance: 8352.385221 [m], (8352.385221, 2.328852133E-8) [m].  
+% Point 1 (del1) to 32 (del1). 
+% Average coordinates: (157127.4098, 10384.96539) [m].  Points: 1, 32 (del1).
 
 % [Sep 16, 2020 10:39 PM]
 % Distance: 12347.59452 [m], (12347.59452, 4.946377885E-8) [m].
