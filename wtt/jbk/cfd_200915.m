d68d2c899b8f5b249e483355d00dd859942e0f54
sbkim <sbkimwind@gmail.com>
2020-10-07 10:14:10 +0900

    s1

--- wtt/jbk/cfd_200915.m
+++ wtt/jbk/cfd_200915.m
@@ -58,7 +58,7 @@ id_pl = false;
 % %%
 sb.s_geo = 'longitudinal';
 sb.Re = 150;
-load(sprintf('tower_%s_turbulent_Re%d', sb.s_geo, sb.Re));
+load(sprintf('tower/tower_%s_turbulent_Re%d', sb.s_geo, sb.Re));
 sb
 
 % %%
@@ -82,7 +82,46 @@ clf
 for kk = 1:2
 for ii = 1:sb.Re_n
     res.sb(kk).Re(ii) = sb.Re_pool(ii);
-    load(sprintf('tower_%s_turbulent_Re%d', res.sb(kk).s_geo, res.sb(kk).Re(ii)));
+    load(sprintf('tower/tower_%s_turbulent_Re%d', res.sb(kk).s_geo, res.sb(kk).Re(ii)));
+    res.sb(kk).res(ii,1:6) = sb.res(ii).C.DLM(1:6);
+    
+    for jj=1:3
+        subplot(1,3,jj)
+        semilogx(sb.U(res.sb(kk).Re(ii)), res.sb(kk).res(ii,jj), res.sb(kk).s_mk{1}, ...
+            'Color',rgb('Navy'), 'MarkerSize', res.sb(kk).s_mk{2})        
+    end
+    if id_pause
+        gcfG;gcfH;gcfLFont;gcfS;%gcfP
+        id_pause = false;
+    end
+    for jj=1:3
+        subplot(1,3,jj)
+        h = semilogx(sb.U(res.sb(kk).Re(ii)), res.sb(kk).res(ii,jj+3), res.sb(kk).s_mk{1}, ...
+            'Color',rgb('Navy'), 'MarkerSize', res.sb(kk).s_mk{2});        
+        h.MarkerFaceColor = h.Color;
+    end
+end
+end
+
+% %%
+clear res
+res.sb(1).s_geo = 'longitudinal';
+res.sb(2).s_geo = 'transverse';
+
+res.sb(1).s_mk{1} = 'o';
+res.sb(2).s_mk{1} = 's';
+res.sb(1).s_mk{2} = 4;
+res.sb(2).s_mk{2} = 5;
+
+id_pause = true;
+figure(1)
+clf
+
+
+for kk = 1:2
+for ii = 1:sb.Re_n
+    res.sb(kk).Re(ii) = sb.Re_pool(ii);
+    load(sprintf('tower/tower_%s_turbulent_Re%d', res.sb(kk).s_geo, res.sb(kk).Re(ii)));
     res.sb(kk).res(ii,1:6) = sb.res(ii).C.DLM(1:6);
     
     for jj=1:3
