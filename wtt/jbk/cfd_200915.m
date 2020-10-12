18d4ced634b2224433f2176bb24002e3a69fc676
sbkim <sbkimwind@gmail.com>
2020-10-12 17:02:10 +0900

    s1

--- wtt/jbk/cfd_200915.m
+++ wtt/jbk/cfd_200915.m
@@ -68,6 +68,8 @@ res.s_c{1} = rgb('Navy');
 res.s_c{2} = rgb('Teal');
 res.s_c{3} = rgb('Crimson');
 
+res.Re_pool = [150 1000 1e4 1e5 2e5];
+res.Re_n = length(res.Re_pool);
 
 id_pause = true;
 figure(1)
@@ -75,8 +77,8 @@ clf
 
 ll = 1;
 for id_geo = 1:3
-for ii = 1:sb.Re_n
-    res.geo(id_geo).sb(ll).Re(ii) = sb.Re_pool(ii);
+for ii = 1:res.Re_n
+    res.geo(id_geo).sb(ll).Re(ii) = res.Re_pool(ii);
     switch id_geo
         case {1,2}
             load(sprintf('tower/tower_%s_turbulent_SST_Re%d', ...
@@ -89,6 +91,7 @@ for ii = 1:sb.Re_n
 
 % if ii == 1
     res.geo(id_geo).sb(ll).BD = [sb.B,sb.D];
+    res.geo(id_geo).sb(ll).BD_F = [sb.B,sb.D]*sb.scale;
 % end
 
     switch id_geo
@@ -98,6 +101,7 @@ for ii = 1:sb.Re_n
             sc = [sb.D/sb.B 1 1];
         % otherwise
     end
+    sc = sb.D/sb.B*[1 1 sb.D/sb.B];
 res.geo(id_geo).sb(ll).res(ii,1:6) = sb.res(ii).C.DLM(1:6)*diag([sc sc]);
 
     for jj=1:3
@@ -120,6 +124,30 @@ res.geo(id_geo).sb(ll).res(ii,1:6) = sb.res(ii).C.DLM(1:6)*diag([sc sc]);
     end
 end
 end
+
+% %%
+res.geo(1).sb.res
+res.geo(2).sb.res
+res.geo(3).sb.res
+
+% %%
+res.geo(1).sb.BD_F
+res.geo(2).sb.BD_F
+res.geo(3).sb.BD_F
+
+% %% [markdown]
+% # Tower
+
+% %% [markdown]
+% - corner
+%     - r/b = 0.01 / [0.045 0.06] = 0.2222 0.1667
+%     - 0.5 0.6
+% - d/b = 6/4.5 = 1.333
+%     - EN: 1.9 *4.5/6/2 = 0.7125
+%     - C: 0.6511
+% - d/b = 4.5/6 = 0.75
+%     - EN: 2.3/2 = 1.15
+%     - C: 1.1243
 
 % %% [markdown]
 % # FINE
